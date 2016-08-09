96edbeb014f5546f306308e81940bbf841d17b6a
John Leen <jleen@saturnvalley.org>
2016-08-09 08:31:14 -0700

    Reorganize source

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,43 @@ let sass = require('gulp-sass');
 let babel = require('gulp-babel');
 let sftp = require('gulp-sftp');
 
+let jsSrc = './js/*.js';
+let sassSrc = './sass/*.scss';
+let uploadTree = './build/**'
+
 gulp.task('sass', () => {
-    return gulp.src('./sass/*.scss')
+    return gulp.src(sassSrc)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./build/css'));
 });
  
 gulp.task('sass:watch', () => {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch(sassSrc, ['sass']);
 });
 
 gulp.task('js', () => {
-    gulp.src('./*.js6')
+    gulp.src(jsSrc)
         .pipe(babel())
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('js:watch', () => {
-    gulp.watch('./*.js6', ['js']);
+    gulp.watch(jsSrc, ['js']);
+});
+
+gulp.task('html', () => {
+    return gulp.src('*.html')
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('watch', ['sass:watch', 'js:watch']);
 
 gulp.task('autoupload', () => {
-    gulp.watch(['./*.html', './*.js', './css/*.css'], ['upload']);
+    gulp.watch([uploadTree], ['upload']);
 });
 
 gulp.task('upload', () => {
-    return gulp.src(['./*.html', './*.js', './css/*.css'], { base: '.' })
+    return gulp.src(uploadTree, { base: './build' })
         .pipe(sftp({
             host: 'blob',
             user: 'jleen',
